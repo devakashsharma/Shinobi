3c6da3453b0d2a34670b91c9d59222f3b8cc5cf0
Akash Sharma <soulakash20002@gmail.com>
2025-01-22 17:10:25 +0530

    Add interactive cursor with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import About from './components/About'
+import CursorFollower from './components/Cursor'
 
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <CursorFollower />
             <Navbar />
             <Hero />
 
