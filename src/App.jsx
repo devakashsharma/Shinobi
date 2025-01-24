ddc5c516ffec878a1d8c78ed604fe212d23133a5
Akash Sharma <soulakash20002@gmail.com>
2025-01-24 15:43:43 +0530

    Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import CursorFollower from './components/Cursor'
 import Skills from './components/Skills'
 import Project from './components/Project'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
     return (
@@ -17,6 +18,7 @@ const App = () => {
             <Skills />
             <Project />
             <Contact />
+            <Footer />
         </main>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="bg-gradient-to-r from-gray-900 to-black text-white py-10">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col md:flex-row justify-center md:justify-around items-center">
+          <h2 className="text-3xl md:text-4xl font-extrabold tracking-wide text-amber-50 mb-6 md:mb-0">
+            Akash Sharma
+          </h2>
+
+          <div>
+            <div className="flex space-x-6 mb-6 md:mb-0">
+              {[
+                {
+                  Icon: FaInstagram,
+                  url: "https://instagram.com/itz_____akash01",
+                },
+                { Icon: FaGithub, url: "https://github.com/devakashsharma" },
+                {
+                  Icon: FaLinkedin,
+                  url: "https://linkedin.com/in/akash-sharma-1b7a73240",
+                },
+                // {
+                //   Icon: FaTwitter,
+                //   url: "https://twitter.com/yourusername",
+                // },
+                {
+                  Icon: SiLeetcode,
+                  url: "https://leetcode.com/devakashsharma",
+                },
+              ].map(({ Icon, url }, index) => (
+                <a
+                  key={index}
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white/70 hover:text-white transition-transform duration-300 hover:scale-125"
+                >
+                  <Icon size={24} />
+                </a>
+              ))}
+            </div>
+            <div className="mt-6 text-center md:text-start">
+              <p className="text-gray-400 text-sm md:text-base">
+                Made with <span className="text-red-500">❤️</span> by Akash
+                Sharma
+              </p>
+              <p className="text-gray-500 text-xs md:text-sm mt-2">
+                © {currentYear} All Rights Reserved
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
